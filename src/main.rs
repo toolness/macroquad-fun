4993a2cf34e531a441aa609eb622c3f78139943b
Atul Varma <varmaa@gmail.com>
2022-08-28 14:57:51 -0400

    Decouple sprite frame animation from fps.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ use sprite::Sprite;
 mod sprite;
 
 const NUM_IDLE_FRAMES: u32 = 8;
+
 const SCALE: f32 = 3.;
+
+const MS_PER_ANIMATION_FRAME: f64 = 100.0;
 
 #[macroquad::main("Fun")]
 async fn main() {
@@ -15,21 +18,20 @@ async fn main() {
         NUM_IDLE_FRAMES,
         SCALE,
     );
-    let mut idle_frame_num = 0;
-    let mut frame_number = 0;
     let x = screen_width() / 2. - idle_sprite.frame_width() / 2.0;
     let y = screen_height() / 2. - idle_sprite.frame_height() / 2.;
 
     loop {
+        let absolute_frame_number = (get_time() * 1000.0 / MS_PER_ANIMATION_FRAME) as u32;
+
         clear_background(GRAY);
 
-        idle_sprite.draw(x, y, WHITE, idle_frame_num);
+        idle_sprite.draw(
+            x,
+            y,
+            WHITE,
+            absolute_frame_number % idle_sprite.num_frames(),
+        );
         next_frame().await;
-
-        frame_number += 1;
-        if frame_number % 10 == 0 {
-            frame_number = 0;
-            idle_frame_num = (idle_frame_num + 1) % idle_sprite.num_frames();
-        }
     }
 }
