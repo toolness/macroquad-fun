074502997f9d0a94c938f2bec18cea37b2327f9d
Atul Varma <varmaa@gmail.com>
2022-08-28 15:00:33 -0400

    Factor out GameSprites struct.

--- src/main.rs
+++ src/main.rs
@@ -9,28 +9,34 @@ const SCALE: f32 = 3.;
 
 const MS_PER_ANIMATION_FRAME: f64 = 100.0;
 
+struct GameSprites {
+    idle: Sprite,
+}
+
 #[macroquad::main("Fun")]
 async fn main() {
-    let idle_sprite = Sprite::new(
-        load_texture("media/Huntress/Sprites/Idle.png")
-            .await
-            .unwrap(),
-        NUM_IDLE_FRAMES,
-        SCALE,
-    );
-    let x = screen_width() / 2. - idle_sprite.frame_width() / 2.0;
-    let y = screen_height() / 2. - idle_sprite.frame_height() / 2.;
+    let sprites = GameSprites {
+        idle: Sprite::new(
+            load_texture("media/Huntress/Sprites/Idle.png")
+                .await
+                .unwrap(),
+            NUM_IDLE_FRAMES,
+            SCALE,
+        ),
+    };
+    let x = screen_width() / 2. - sprites.idle.frame_width() / 2.0;
+    let y = screen_height() / 2. - sprites.idle.frame_height() / 2.;
 
     loop {
         let absolute_frame_number = (get_time() * 1000.0 / MS_PER_ANIMATION_FRAME) as u32;
 
         clear_background(GRAY);
 
-        idle_sprite.draw(
+        sprites.idle.draw(
             x,
             y,
             WHITE,
-            absolute_frame_number % idle_sprite.num_frames(),
+            absolute_frame_number % sprites.idle.num_frames(),
         );
         next_frame().await;
     }
