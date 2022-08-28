ad3bf89ee499efe2b44461fb449bf0728d0826df
Atul Varma <varmaa@gmail.com>
2022-08-28 18:05:54 -0400

    Add jumping.

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,10 @@ const IDLE_FRAME_FEET_Y: f32 = 96.0;
 
 const GROUND_HEIGHT: f32 = 8.0 * SCALE;
 
+const GRAVITY: f32 = 1500.0;
+
+const JUMP_VELOCITY: f32 = 600.0;
+
 struct GameSprites {
     idle: Sprite,
     run: Sprite,
@@ -55,7 +59,9 @@ async fn main() {
     let mut x = screen_width() / 2. - sprites.idle.frame_width() / 2.0;
     let ground_y = screen_height() - GROUND_HEIGHT;
     let sprite_ground_y = ground_y - IDLE_FRAME_FEET_Y * SCALE;
-    let y = sprite_ground_y;
+    let mut y = sprite_ground_y;
+    let mut is_in_air = false;
+    let mut velocity = Vec2::new(0., 0.);
     let mut last_frame_time = get_time();
     let mut is_facing_left = false;
     let mut debug_mode = false;
@@ -75,24 +81,53 @@ async fn main() {
 
         draw_rectangle(0., ground_y, screen_width(), GROUND_HEIGHT, DARKGRAY);
 
+        let is_pressing_right = is_key_down(KeyCode::D);
+        let is_pressing_left = is_key_down(KeyCode::A);
+
+        if is_key_pressed(KeyCode::Space) && !is_in_air {
+            let x_velocity = if is_pressing_left {
+                -RUN_SPEED as f32
+            } else if is_pressing_right {
+                RUN_SPEED as f32
+            } else {
+                0.
+            };
+            velocity = Vec2::new(x_velocity, -JUMP_VELOCITY);
+            is_in_air = true
+        } else if is_in_air {
+            if y >= sprite_ground_y {
+                is_in_air = false;
+                velocity = Vec2::new(0., 0.);
+            } else {
+                velocity.y += GRAVITY * time_since_last_frame as f32;
+            }
+        }
+
+        x += velocity.x * time_since_last_frame as f32;
+        y += velocity.y * time_since_last_frame as f32;
+
         let sprite: &Sprite;
 
-        if is_key_down(KeyCode::D) || is_key_down(KeyCode::A) {
-            sprite = &sprites.run;
-            let run_amount = (time_since_last_frame * RUN_SPEED) as f32;
-            if is_key_down(KeyCode::D) {
-                is_facing_left = false;
-                x += run_amount;
+        if is_in_air {
+            if velocity.y >= 0. {
+                sprite = &sprites.fall;
             } else {
-                is_facing_left = true;
-                x -= run_amount;
+                sprite = &sprites.jump;
             }
-        } else if is_key_down(KeyCode::W) {
-            sprite = &sprites.jump;
-        } else if is_key_down(KeyCode::S) {
-            sprite = &sprites.fall;
         } else {
-            sprite = &sprites.idle;
+            if is_pressing_left || is_pressing_right {
+                sprite = &sprites.run;
+                let run_amount = (time_since_last_frame * RUN_SPEED) as f32;
+                if is_pressing_right {
+                    is_facing_left = false;
+                    x += run_amount;
+                } else {
+                    is_facing_left = true;
+                    x -= run_amount;
+                }
+            } else {
+                sprite = &sprites.idle;
+            }
         }
 
         sprite.draw_ex(
