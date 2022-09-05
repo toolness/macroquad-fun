6cfddc5b9c2594d7ebbd67e9ac8dfc681420a7bd
Atul Varma <varmaa@gmail.com>
2022-09-05 15:22:51 -0400

    Load bounding box for FlyingEye.

--- src/game_sprites.rs
+++ src/game_sprites.rs
@@ -15,6 +15,7 @@ pub struct HuntressSprites {
 
 pub struct FlyingEyeSprites {
     pub flight: Sprite,
+    pub flight_bbox: Rect,
 }
 
 pub struct GameSprites {
@@ -31,18 +32,23 @@ fn get_slice(slices: &HashMap<String, Rect>, name: &str) -> Result<Rect> {
 }
 
 pub async fn load_game_sprites(scale: f32) -> Result<GameSprites> {
-    let huntress_idle_slices = load_aseprite_slices("media/Huntress/Idle.json", scale).await?;
-
     let sprites = GameSprites {
         huntress: HuntressSprites {
             idle: Sprite::new(load_texture("media/Huntress/Idle.png").await?, 8, scale),
             run: Sprite::new(load_texture("media/Huntress/Run.png").await?, 8, scale),
             jump: Sprite::new(load_texture("media/Huntress/Jump.png").await?, 2, scale),
             fall: Sprite::new(load_texture("media/Huntress/Fall.png").await?, 2, scale),
-            idle_bbox: get_slice(&huntress_idle_slices, "idle_bounding_box")?,
+            idle_bbox: get_slice(
+                &load_aseprite_slices("media/Huntress/Idle.json", scale).await?,
+                "idle_bounding_box",
+            )?,
         },
         flying_eye: FlyingEyeSprites {
             flight: Sprite::new(load_texture("media/FlyingEye/Flight.png").await?, 8, scale),
+            flight_bbox: get_slice(
+                &load_aseprite_slices("media/FlyingEye/Flight.json", scale).await?,
+                "flight_bounding_box",
+            )?,
         },
     };
 
