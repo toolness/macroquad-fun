74b753d1809529b83efaa3535f65687d68c70903
Atul Varma <varmaa@gmail.com>
2022-09-05 10:38:22 -0400

    Parse FieldInstance from LDtk.

--- src/ldtk.rs
+++ src/ldtk.rs
@@ -79,18 +79,52 @@ pub struct EntityInstance {
     /// Grid-based coordinates (`[x,y]` format)
     #[serde(rename = "__grid")]
     pub grid: Vec<i64>,
+
     /// Entity definition identifier
     #[serde(rename = "__identifier")]
     pub identifier: String,
+
     /// Entity height in pixels. For non-resizable entities, it will be the same as Entity
     /// definition.
     pub height: i64,
+
     /// Unique instance identifier
     pub iid: String,
+
     /// Pixel coordinates (`[x,y]` format) in current level coordinate space. Don't forget
     /// optional layer offsets, if they exist!
     pub px: Vec<i64>,
+
     /// Entity width in pixels. For non-resizable entities, it will be the same as Entity
     /// definition.
     pub width: i64,
+
+    /// An array of all custom fields and their values.
+    #[serde(rename = "fieldInstances")]
+    pub field_instances: Vec<FieldInstance>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct FieldInstance {
+    /// Field definition identifier
+    #[serde(rename = "__identifier")]
+    pub identifier: String,
+
+    /// Type of the field, such as `Int`, `Float`, `String`, `Enum(my_enum_name)`, `Bool`,
+    /// etc.<br/>  NOTE: if you enable the advanced option **Use Multilines type**, you will have
+    /// "*Multilines*" instead of "*String*" when relevant.
+    #[serde(rename = "__type")]
+    pub field_instance_type: String,
+
+    /// Actual value of the field instance. The value type varies, depending on `__type`:<br/>
+    /// - For **classic types** (ie. Integer, Float, Boolean, String, Text and FilePath), you
+    /// just get the actual value with the expected type.<br/>   - For **Color**, the value is an
+    /// hexadecimal string using "#rrggbb" format.<br/>   - For **Enum**, the value is a String
+    /// representing the selected enum value.<br/>   - For **Point**, the value is a
+    /// [GridPoint](#ldtk-GridPoint) object.<br/>   - For **Tile**, the value is a
+    /// [TilesetRect](#ldtk-TilesetRect) object.<br/>   - For **EntityRef**, the value is an
+    /// [EntityReferenceInfos](#ldtk-EntityReferenceInfos) object.<br/><br/>  If the field is an
+    /// array, then this `__value` will also be a JSON array.
+    #[serde(rename = "__value")]
+    pub value: Option<serde_json::Value>,
 }
