368a1607bc05892d87c4c2b3177bfaf9b6f422f5
Atul Varma <varmaa@gmail.com>
2022-09-06 21:34:00 -0400

    Factor out Player struct.

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,33 @@ mod level;
 mod running;
 mod sprite;
 
+pub struct Player {
+    pub pos: Vec2,
+    pub is_in_air: bool,
+    pub velocity: Vec2,
+    pub is_facing_left: bool,
+    pub relative_bbox: Rect,
+}
+
+impl Player {
+    pub fn new(start_rect: Rect, relative_bbox: Rect) -> Self {
+        Player {
+            pos: Vec2::new(
+                start_rect.left() - relative_bbox.x,
+                start_rect.bottom() - relative_bbox.bottom(),
+            ),
+            relative_bbox,
+            is_in_air: false,
+            velocity: Vec2::new(0., 0.),
+            is_facing_left: false,
+        }
+    }
+
+    pub fn bbox(&self) -> Rect {
+        self.relative_bbox.offset(self.pos)
+    }
+}
+
 #[macroquad::main("Fun")]
 async fn main() {
     let config = load_config("media/config.json").await.unwrap();
@@ -34,17 +61,12 @@ async fn main() {
         .player_start()
         .expect("World must define a player start position");
     let sprites = load_game_sprites(sprite_scale).await.unwrap();
-    let player_relative_bbox = sprites.huntress.idle_bbox;
 
     request_new_screen_size(config.screen_width, config.screen_height);
     next_frame().await;
 
-    let mut x = player_start.left() - player_relative_bbox.x;
-    let mut y = player_start.bottom() - player_relative_bbox.bottom();
-    let mut is_in_air = false;
-    let mut velocity = Vec2::new(0., 0.);
+    let mut player = Player::new(player_start, sprites.huntress.idle_bbox);
     let mut last_frame_time = get_time();
-    let mut is_facing_left = false;
     let mut debug_mode = false;
     let mut run_manager = RunManager::new();
 
@@ -58,16 +80,13 @@ async fn main() {
 
         // If the player isn't mostly inside the current level, change levels.
         {
-            let pos = Vec2::new(x, y);
-            let bbox = player_relative_bbox.offset(pos);
-            if !level.contains_majority_of(&bbox) {
-                let world_pos = level.to_world_coords(&pos);
+            if !level.contains_majority_of(&player.bbox()) {
+                let world_pos = level.to_world_coords(&player.pos);
                 if let Some((new_level, new_pos)) =
-                    world.find_level_containing_majority_of(&world_pos, &player_relative_bbox)
+                    world.find_level_containing_majority_of(&world_pos, &player.relative_bbox)
                 {
                     level = new_level;
-                    x = new_pos.x;
-                    y = new_pos.y;
+                    player.pos = new_pos;
                 }
             }
         }
@@ -75,7 +94,7 @@ async fn main() {
         // Position the camera.
         let camera_rect: Rect;
         {
-            let bbox = player_relative_bbox.offset(Vec2::new(x, y));
+            let bbox = player.bbox();
             let bbox_center = Vec2::new(bbox.x + bbox.w / 2., bbox.y + bbox.h / 2.);
             camera_rect = calculate_camera_rect(&config, &bbox_center, &level.pixel_bounds());
             set_camera(&Camera2D::from_display_rect(camera_rect));
@@ -96,34 +115,35 @@ async fn main() {
         );
         let mut x_impulse: f32 = 0.;
 
-        if is_in_air {
-            if is_key_down(KeyCode::Space) && velocity.y < 0. {
-                velocity.y -= config.long_jump_keypress_extra_force * time_since_last_frame as f32;
+        if player.is_in_air {
+            if is_key_down(KeyCode::Space) && player.velocity.y < 0. {
+                player.velocity.y -=
+                    config.long_jump_keypress_extra_force * time_since_last_frame as f32;
             }
-            velocity.y += config.gravity * time_since_last_frame as f32;
+            player.velocity.y += config.gravity * time_since_last_frame as f32;
             if run_manager.is_running() {
-                velocity.x = run_manager.run_speed();
+                player.velocity.x = run_manager.run_speed();
             }
         } else {
             if is_key_pressed(KeyCode::Space) {
-                velocity = Vec2::new(run_manager.run_speed(), -config.jump_velocity);
-                is_in_air = true
+                player.velocity = Vec2::new(run_manager.run_speed(), -config.jump_velocity);
+                player.is_in_air = true
             } else {
                 x_impulse = run_manager.run_speed();
             }
         }
 
-        let player_prev_bbox = player_relative_bbox.offset(Vec2::new(x, y));
-        x += (velocity.x + x_impulse) * time_since_last_frame as f32;
-        y += velocity.y * time_since_last_frame as f32;
+        let player_prev_bbox = player.bbox();
+        player.pos.x += (player.velocity.x + x_impulse) * time_since_last_frame as f32;
+        player.pos.y += player.velocity.y * time_since_last_frame as f32;
 
         let mut is_on_any_surface_this_frame = false;
 
         loop {
             let player_actor = Actor {
                 prev_bbox: player_prev_bbox,
-                bbox: player_relative_bbox.offset(Vec2::new(x, y)),
-                velocity,
+                bbox: player.bbox(),
+                velocity: player.velocity,
             };
             let mut displacement_occurred = false;
             for collider in level.iter_colliders(&player_actor.bbox) {
@@ -132,12 +152,11 @@ async fn main() {
                         is_on_any_surface_this_frame = true;
                     }
                     if let Some(new_velocity) = collision.new_velocity {
-                        velocity = new_velocity;
+                        player.velocity = new_velocity;
                     }
 
                     if collision.displacement.x != 0. || collision.displacement.y != 0. {
-                        x += collision.displacement.x;
-                        y += collision.displacement.y;
+                        player.pos += collision.displacement;
                         displacement_occurred = true;
                         break;
                     }
@@ -150,10 +169,10 @@ async fn main() {
 
         if is_on_any_surface_this_frame {
             // The player just landed (or remains on the ground).
-            is_in_air = false;
-        } else if !is_in_air {
+            player.is_in_air = false;
+        } else if !player.is_in_air {
             // The player just fell off a ledge.
-            is_in_air = true;
+            player.is_in_air = true;
         }
 
         // Draw NPCs.
@@ -178,8 +197,8 @@ async fn main() {
 
         let sprite: &Sprite;
 
-        if is_in_air {
-            if velocity.y >= 0. {
+        if player.is_in_air {
+            if player.velocity.y >= 0. {
                 sprite = &sprites.huntress.fall;
             } else {
                 sprite = &sprites.huntress.jump;
@@ -187,24 +206,24 @@ async fn main() {
         } else {
             if x_impulse != 0. {
                 sprite = &sprites.huntress.run;
-                is_facing_left = x_impulse < 0.;
+                player.is_facing_left = x_impulse < 0.;
             } else {
                 sprite = &sprites.huntress.idle;
             }
         }
 
         sprite.draw_ex(
-            x,
-            y,
+            player.pos.x,
+            player.pos.y,
             absolute_frame_number % sprite.num_frames(),
             SpriteDrawParams {
-                flip_x: is_facing_left,
+                flip_x: player.is_facing_left,
                 ..Default::default()
             },
         );
 
         // Draw level text.
-        let player_bbox = player_relative_bbox.offset(Vec2::new(x, y));
+        let player_bbox = player.bbox();
         if let Some(text) = level.get_text(&player_bbox) {
             let mut y = camera_rect.y + 128.;
             for line in text {
@@ -221,7 +240,7 @@ async fn main() {
             debug_mode = !debug_mode;
         }
         if debug_mode {
-            sprite.draw_debug_rect(x, y, GREEN);
+            sprite.draw_debug_rect(player.pos.x, player.pos.y, GREEN);
             draw_rect_lines(&player_bbox, 2., PURPLE);
             for collider in level.iter_colliders(&level.pixel_bounds()) {
                 collider.draw_debug_rect(PURPLE);
