c4b3ccf9c9a67549e4631c5f8ad2fefbf57fb779
Atul Varma <varmaa@gmail.com>
2022-10-25 23:10:34 -0400

    Simplify DebugMode.

--- src/debug_mode.rs
+++ src/debug_mode.rs
@@ -7,27 +7,24 @@ use crate::{config::config, fps::FpsCounter, level_runtime::LevelRuntime};
 const DEBUG_TEXT_CAPACITY: usize = 3000;
 
 pub struct DebugMode {
-    debug_text_lines: Option<String>,
+    text: String,
 }
 
 impl Default for DebugMode {
     fn default() -> Self {
         DebugMode {
-            debug_text_lines: None,
+            text: String::with_capacity(DEBUG_TEXT_CAPACITY),
         }
     }
 }
 
 impl DebugMode {
     pub fn update(&mut self, runtime: &LevelRuntime, fps: &FpsCounter) -> Result<()> {
-        let text = self
-            .debug_text_lines
-            .get_or_insert_with(|| String::with_capacity(DEBUG_TEXT_CAPACITY));
-        text.clear();
+        self.text.clear();
 
-        writeln!(text, "fps: {}", fps.value())?;
+        writeln!(self.text, "fps: {}", fps.value())?;
 
-        runtime.generate_debug_text(text)?;
+        runtime.generate_debug_text(&mut self.text)?;
 
         Ok(())
     }
@@ -35,15 +32,13 @@ impl DebugMode {
     pub fn draw(&self, runtime: &LevelRuntime) {
         runtime.draw_debug_layer();
 
-        if let Some(text) = &self.debug_text_lines {
-            let font_size = config().debug_text_size;
-            let margin = 32.;
-            let x = margin;
-            let mut y = margin;
-            for line in text.split("\n") {
-                draw_text(line, x, y, font_size, YELLOW);
-                y += font_size;
-            }
+        let font_size = config().debug_text_size;
+        let margin = 32.;
+        let x = margin;
+        let mut y = margin;
+        for line in self.text.split("\n") {
+            draw_text(line, x, y, font_size, YELLOW);
+            y += font_size;
         }
     }
 }
