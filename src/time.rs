fb0fa2c3046644138cff46c7189e4ef166784034
Atul Varma <varmaa@gmail.com>
2022-10-29 12:16:53 -0400

    Add tests.

--- src/time.rs
+++ src/time.rs
@@ -14,8 +14,9 @@ use crate::{config::config, sprite_renderer::SpriteRenderer};
 ///
 /// (Note that if we actually want to play the game in slow motion, we can
 /// set this to a ridiculously high value!)
-const MAX_TIME_BETWEEN_FRAMES: f64 = 1. / 30.;
+const DEFAULT_MAX_TIME_BETWEEN_FRAMES: f64 = 1. / 30.;
 
+#[derive(Debug, PartialEq)]
 pub struct GameTime {
     pub now: f64,
     pub absolute_frame_number: u64,
@@ -34,16 +35,34 @@ pub struct FixedGameTime {
     start: f64,
     frame_duration: f64,
     excess_time_offset: f64,
+    max_time_between_frames: f64,
+    ms_per_animation_frame: f64,
 }
 
 impl FixedGameTime {
     pub fn new(fixed_frame_rate: u64, now: f64) -> Self {
+        Self::new_ex(
+            fixed_frame_rate,
+            now,
+            DEFAULT_MAX_TIME_BETWEEN_FRAMES,
+            config().ms_per_animation_frame,
+        )
+    }
+
+    fn new_ex(
+        fixed_frame_rate: u64,
+        now: f64,
+        max_time_between_frames: f64,
+        ms_per_animation_frame: f64,
+    ) -> Self {
         FixedGameTime {
             now,
             start: now,
             frames_so_far: 0,
             frame_duration: 1. / (fixed_frame_rate as f64),
             excess_time_offset: 0.,
+            max_time_between_frames,
+            ms_per_animation_frame,
         }
     }
 
@@ -51,10 +70,10 @@ impl FixedGameTime {
         let last_frame_time = self.now;
         self.now = now - self.excess_time_offset;
         let time_since_last_frame = self.now - last_frame_time;
-        if time_since_last_frame > MAX_TIME_BETWEEN_FRAMES {
+        if time_since_last_frame > self.max_time_between_frames {
             // Our "real" time as reported from macroquad is has now deviated from our
             // in-game concept of time, so adjust accordingly.
-            let delta = time_since_last_frame - MAX_TIME_BETWEEN_FRAMES;
+            let delta = time_since_last_frame - self.max_time_between_frames;
             self.excess_time_offset += delta;
             self.now -= delta;
         }
@@ -68,7 +87,7 @@ impl FixedGameTime {
             let synthetic_now = self.start + (self.frames_so_far as f64) * self.frame_duration;
             Some(GameTime {
                 now: synthetic_now,
-                absolute_frame_number: (synthetic_now * 1000.0 / config().ms_per_animation_frame)
+                absolute_frame_number: (synthetic_now * 1000.0 / self.ms_per_animation_frame)
                     as u64,
                 time_since_last_frame: self.frame_duration,
             })
@@ -91,5 +110,57 @@ impl<'a> Iterator for FixedFrameIterator<'a> {
 
     fn next(&mut self) -> Option<Self::Item> {
         self.time.next_fixed_frame()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{FixedGameTime, GameTime};
+
+    fn get_frames(fixed: &mut FixedGameTime) -> Vec<GameTime> {
+        fixed.iter_fixed_frames().collect::<Vec<GameTime>>()
+    }
+
+    #[test]
+    fn test_it_works() {
+        let mut fixed = FixedGameTime::new_ex(1, 0., 2., 100.);
+        assert_eq!(get_frames(&mut fixed), vec![]);
+        fixed.update(1.0);
+        assert_eq!(
+            get_frames(&mut fixed),
+            vec![GameTime {
+                now: 1.0,
+                absolute_frame_number: 10,
+                time_since_last_frame: 1.0
+            }]
+        );
+        assert_eq!(get_frames(&mut fixed), vec![]);
+        fixed.update(1.5);
+        assert_eq!(get_frames(&mut fixed), vec![]);
+        fixed.update(2.2);
+        assert_eq!(
+            get_frames(&mut fixed),
+            vec![GameTime {
+                now: 2.0,
+                absolute_frame_number: 20,
+                time_since_last_frame: 1.0
+            }]
+        );
+        fixed.update(4.1);
+        assert_eq!(
+            get_frames(&mut fixed),
+            vec![
+                GameTime {
+                    now: 3.0,
+                    absolute_frame_number: 30,
+                    time_since_last_frame: 1.0
+                },
+                GameTime {
+                    now: 4.0,
+                    absolute_frame_number: 40,
+                    time_since_last_frame: 1.0
+                }
+            ]
+        );
     }
 }
