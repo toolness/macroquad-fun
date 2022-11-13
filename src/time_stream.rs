bac5e3ef6f4015d92bb8ee067b7d77ce7c97fc96
Atul Varma <varmaa@gmail.com>
2022-11-13 06:54:03 -0500

    Add FixedFpsTimeStream.

--- src/time_stream.rs
+++ src/time_stream.rs
@@ -15,3 +15,22 @@ impl Iterator for RealTimeStream {
 pub fn create_real_time_stream() -> TimeStream {
     Box::new(RealTimeStream)
 }
+
+pub struct FixedFpsTimeStream {
+    next_time: f64,
+    fps: u64,
+}
+
+impl Iterator for FixedFpsTimeStream {
+    type Item = f64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let time = self.next_time;
+        self.next_time += 1.0 / self.fps as f64;
+        Some(time)
+    }
+}
+
+pub fn create_fixed_fps_time_stream(fps: u64) -> TimeStream {
+    Box::new(FixedFpsTimeStream { next_time: 0., fps })
+}
