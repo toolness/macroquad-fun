d1685a57cd72a0bfe0558cb04fcd1a3734e6ea4c
Atul Varma <varmaa@gmail.com>
2023-01-01 17:55:23 -0500

    Improve time.rs docs.

--- src/time.rs
+++ src/time.rs
@@ -2,18 +2,6 @@ use std::iter::Fuse;
 
 use crate::{config::config, sprite_renderer::SpriteRenderer};
 
-/// The maximum time we'll process between drawing frames. This prevents
-/// us from spending too long "catching up" with the time elapsed since
-/// the last frame. It also ensures resonable behavior in situations where
-/// e.g. the user puts their computer to sleep, or Macroquad's event
-/// loop takes a really long time to get back to us (see e.g.
-/// https://github.com/toolness/macroquad-fun/issues/4).
-///
-///  This does mean that really slow devices will appear to
-/// run in slow motion.
-///
-/// (Note that if we actually want to play the game in slow motion, we can
-/// set this to a ridiculously high value!)
 const DEFAULT_MAX_TIME_BETWEEN_FRAMES: f64 = 1. / 30.;
 
 #[derive(Debug, PartialEq)]
@@ -31,13 +19,46 @@ impl GameTime {
 
 #[derive(Clone, Copy)]
 pub struct FixedGameTime {
+    /// The current game time, in seconds, taking into account `excess_time_offset`.
+    /// This means that e.g. if the game was paused for 3 seconds, this will not
+    /// include those 3 seconds.
     now: f64,
+
+    /// How many frames we've processed so far.
     frames_so_far: u64,
+
+    /// The time, in seconds, when we started the game.
     start: f64,
+
+    /// How long each frame lasts. This is basically 1 / fixed_frame_rate.
     frame_duration: f64,
+
+    /// The difference between the current game time and real-world time due to
+    /// e.g. the game being paused.
     excess_time_offset: f64,
+
+    /// The maximum amount of time, in seconds, that we'll allow to elapse from one
+    /// game frame to the next. If more than this amount of real-world time passes
+    /// between frames, we'll "truncate" it to this maximum to avoid undesirable
+    /// effects.
+    ///
+    /// For example, this prevents us from spending too long "catching up" with the
+    /// time elapsed since the last frame. It also ensures resonable behavior in
+    /// situations where e.g. the user puts their computer to sleep, or Macroquad's event
+    /// loop takes a really long time to get back to us (see e.g.
+    /// https://github.com/toolness/macroquad-fun/issues/4).
+    ///
+    /// This does mean that really slow devices will appear to
+    /// run in slow motion.
+    ///
+    /// (Note that if we actually want to play the game in slow motion, we can
+    /// set this to a ridiculously high value!)
     max_time_between_frames: f64,
+
+    /// How many milliseconds each sprite animation frame lasts, by default.
     ms_per_animation_frame: f64,
+
+    /// If we were paused, this is the *real-world* time at which we were paused.
     time_when_paused: Option<f64>,
 }
 
