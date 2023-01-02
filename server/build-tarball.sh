a3a9ae9546d7d70a2e1026adb32cba74c59c703c
Atul Varma <varmaa@gmail.com>
2023-01-02 15:27:16 -0500

    Rename server executable.

--- server/build-tarball.sh
+++ server/build-tarball.sh
@@ -4,7 +4,7 @@ cargo build --release && \
   rm -rf dist && \
   mkdir -p dist/macroquad-fun && \
   cd dist && \
-  cp ../target/release/macroquad-fun macroquad-fun && \
+  cp ../target/release/macroquad-fun-server macroquad-fun && \
   tar -zcvf macroquad-fun.tar.gz macroquad-fun && \
   rm -rf macroquad-fun && \
   echo "Wrote dist/macroquad-fun.tar.gz."
