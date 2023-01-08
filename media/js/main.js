b4d0cc2e52f91054ffba53c37d90ceddb0d93c7c
Atul Varma <varmaa@gmail.com>
2023-01-08 11:03:56 -0500

    Updates

--- media/js/main.js
+++ media/js/main.js
@@ -1,13 +1,13 @@
 import * as analyticsDialog from "./analytics-dialog.js";
 
-await analyticsDialog.maybeShowDialog();
-
 const config = await (await fetch("media/config.json")).json();
 const width = config.screen_width * config.sprite_scale;
 const height = config.screen_height * config.sprite_scale;
 const canvas = document.getElementById("glcanvas");
 
 canvas.style = `width: ${width}px; height: ${height}px`;
+
+await analyticsDialog.maybeShowDialog();
 
 const DEBUG = false;
 
